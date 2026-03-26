76cc0bc27cdf15d50e0d69e2d7f13a74b90c041f
itsWindows11 <itswin11@outlook.com>
2026-03-26 11:39:41 +0200

    Added folder implementation that allows for item projection

---
+++ app/src/main/kotlin/com/itswin11/ktabstractstorage/ItemsOverrideFolder.kt
@@ -0,0 +1,24 @@
+package com.itswin11.ktabstractstorage
+
+import com.itswin11.ktabstractstorage.enums.StorableType
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Overrides the items returned by a wrapped [Folder]'s [Folder.getItemsAsync].
+ *
+ * @param folder The folder being overridden.
+ * @param itemsOverrideFunc The function used to override the items yielded by [getItemsAsync].
+ */
+class ItemsOverrideFolder(
+    val folder: Folder,
+    val itemsOverrideFunc: (Flow<StorableChild>) -> Flow<StorableChild>,
+) : Folder {
+    override val id: String
+        get() = folder.id
+
+    override val name: String
+        get() = folder.name
+
+    override fun getItemsAsync(type: StorableType): Flow<StorableChild> =
+        itemsOverrideFunc(folder.getItemsAsync(type))
+}

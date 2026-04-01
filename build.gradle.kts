d88abb64ce51c4cdf840863d103f21b7d030f326
itsWindows11 <itswin11@outlook.com>
2026-04-01 22:13:15 +0200

    Cleaned up repeated Kotlin plugin loading

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") apply false
+    id("org.jetbrains.kotlin.multiplatform") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("com.android.library") apply false
+    id("com.vanniktech.maven.publish") apply false
+}
+
 allprojects {
     group = providers.gradleProperty("POM_GROUP_ID").getOrElse("io.github.kt-abstract-storage")
     version = providers.gradleProperty("VERSION_NAME").getOrElse("0.1.0-SNAPSHOT")
